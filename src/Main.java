98971df348b6b113467ab70f922847dbe3a5cbd6
Evgeniy <evgen03010@gmail.com>
2026-05-01 16:20:24 +0300

    Тест2

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,11 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("i = " + i);
             System.out.println("i = " + i);
+            System.out.println("i = " + i);
+            System.out.println("i = " + i);
+            System.out.println("i = " + i);
+            System.out.println("i = " + i);
+            System.out.println("i = " + i);
         }
     }
 }
